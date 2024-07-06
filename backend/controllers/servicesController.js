ffa84d4d9ebb513bc5991ace5496e69fedafd02c
javierBorjas <cristman11@gmail.com>
2024-07-06 19:48:25 -0300

    Leer los datos enviados hacia la API

--- backend/controllers/servicesController.js
+++ backend/controllers/servicesController.js
@@ -1,7 +1,13 @@
 import { services } from '../data/beautyServices.js'
 
 const  createService = async (req, res) => {
-    console.log('Desde createService')
+    if(Object.values(req.body).includes('')) {
+        const error = new Error('Todos los campos son obligatorios')
+
+        return res.status(400).json({
+            msg: error.message
+        })
+    }
 }
 
 const getServices = (req, res) => {

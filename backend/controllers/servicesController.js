357054beacddba66327ee847bff2b335a0477bd7
javierBorjas <cristman11@gmail.com>
2024-07-06 20:48:44 -0300

    Obtener un Servicio por su ID

--- backend/controllers/servicesController.js
+++ backend/controllers/servicesController.js
@@ -1,4 +1,6 @@
 import { services } from '../data/beautyServices.js'
+import mongoose from 'mongoose'
+import Services from '../models/Services.js'
 
 const  createService = async (req, res) => {
     if(Object.values(req.body).includes('')) {
@@ -8,13 +10,51 @@ const  createService = async (req, res) => {
             msg: error.message
         })
     }
+
+    try {
+        const service = new Services(req.body)
+        await service.save()
+
+        res.json({
+            msg: 'El Servicio se creo correctamente'
+        })
+    } catch (error) {
+       console.log(error) 
+    }
 }
+
 
 const getServices = (req, res) => {
     res.json(services)
 }
 
+const getServiceById = async (req, res) => {
+    const { id } = req.params
+   // Validar un object id
+   if(!mongoose.Types.ObjectId.isValid(id)) {
+        const error = new Error('El ID no es válido')
+
+    return res.status(400).json({
+        msg: error.message
+    })
+}
+
+   // Validar que exista
+   const service = await Services.findById(id)
+   if(!service) {
+    const error = new Error('El Servicio no existe')
+
+    return res.status(404).json({
+        msg: error.message
+    })
+}
+
+   // Mostrar el servicio
+   res.json(service)
+}
+
 export {
     createService,
-    getServices
+    getServices,
+    getServiceById
 }
--- backend/routes/servicesRoutes.js
+++ backend/routes/servicesRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { createService, getServices } from '../controllers/servicesController.js' 
+import { createService, getServices, getServiceById } from '../controllers/servicesController.js' 
 
 const router = express.Router()
 
 router.post('/', createService)
 router.get('/', getServices)
+router.get('/:id', getServiceById)
 
 export default router

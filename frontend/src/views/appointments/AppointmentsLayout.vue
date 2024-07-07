9522dea2e6f05190b1ff03377a6e5b93b806d056
javierBorjas <cristman11@gmail.com>
2024-07-06 23:51:27 -0300

    Definiendo Rutas y Vistas para Nuevas citas

---
+++ frontend/src/views/appointments/AppointmentsLayout.vue
@@ -0,0 +1,16 @@
+import { RouterView } from 'vue-router';
+<script setup>
+        
+</script>
+
+
+<template>
+    <div>
+        <h1>AppointmentLayout</h1>
+    </div>
+
+    <main>
+        <RouterView />
+    </main>
+</template>
+
---
+++ frontend/src/views/appointments/NewAppointmentsLayout.vue
@@ -0,0 +1,11 @@
+import { RouterView } from 'vue-router';
+<script setup>
+
+</script>
+
+
+<template>
+    <h1>New Appointment Layout</h1>
+
+    <RouterView />
+</template>

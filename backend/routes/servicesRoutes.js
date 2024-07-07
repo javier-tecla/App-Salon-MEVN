4695c17f93bf5547fa7396e8d13625a420cb348b
javierBorjas <cristman11@gmail.com>
2024-07-06 21:03:54 -0300

    Actualización de Servicios

--- backend/routes/servicesRoutes.js
+++ backend/routes/servicesRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { createService, getServices, getServiceById } from '../controllers/servicesController.js' 
+import { createService, getServices, getServiceById, updateService } from '../controllers/servicesController.js' 
 
 const router = express.Router()
 
 router.post('/', createService)
 router.get('/', getServices)
 router.get('/:id', getServiceById)
+router.put('/:id', updateService)
 
 export default router

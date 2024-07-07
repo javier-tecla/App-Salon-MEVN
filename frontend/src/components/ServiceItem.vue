ae870dce8b94755f67fa385396130de8b518beee
javierBorjas <cristman11@gmail.com>
2024-07-07 18:01:41 -0300

    Evitar Registros Duplicados

--- frontend/src/components/ServiceItem.vue
+++ frontend/src/components/ServiceItem.vue
@@ -1,6 +1,6 @@
 <script setup>
-    import { formatCurrency } from '../helpers';
-    import { useAppointmentsStore } from '../stores/appointments';
+    import { formatCurrency } from '../helpers'
+    import { useAppointmentsStore } from '../stores/appointments'
 
     const appointments = useAppointmentsStore()
 
@@ -14,10 +14,15 @@
 
 <template>
     <div 
-        class="p-5 space-y-5 rounded-lg cuursor-pointer bg-white"
+        class="p-5 space-y-5 rounded-lg cursor-pointer"
+        :class="appointments.isServiceSelected(service._id) ? 'bg-blue-500 text-white' : 'bg-white'"
         @click="appointments.onServiceSelected(service)"
     >
-        <p class="text-2xl font-light">{{  service.name }}</p>
-        <p class="text-4xl font-black text-blue-600">{{ formatCurrency( service.price ) }}</p>
+        <p class="text-2xl font-light">{{ service.name }}</p>
+        <p 
+            class="text-4xl font-black "
+            :class="appointments.isServiceSelected(service._id) ? 'text-white' : 'text-blue-600'"
+        
+        >{{ formatCurrency( service.price ) }}</p>
     </div>
 </template>
--- frontend/src/stores/appointments.js
+++ frontend/src/stores/appointments.js
@@ -1,11 +1,28 @@
+import { ref, computed } from "vue";
 import { defineStore } from "pinia";
 
 export const useAppointmentsStore = defineStore('appointments', () => {
 
+    const services = ref([])
+
     function onServiceSelected(service) {
-        console.log(service)
+        if(services.value.some(selectedService => selectedService._id === service._id)) {
+            services.value = services.value.filter( selectedService => selectedService._id !== service._id )
+        } else {
+            if(services.value.length === 2) {
+                alert('M}áximo 2 servicios por cita')
+                return
+            }
+            services.value.push(service) 
+        }
+        
     }
+        const isServiceSelected = computed(() => {
+            return (id) => services.value.some( service => service._id === id )
+        })
+
     return {
-        onServiceSelected
+        onServiceSelected,
+        isServiceSelected
     }
 })

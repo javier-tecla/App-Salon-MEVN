04df4d1ae1f3b8b2be65bced69d148bd473d400f
javierBorjas <cristman11@gmail.com>
2024-07-06 21:59:18 -0300

    Eliminando Multiples registros

--- backend/data/seed.js
+++ backend/data/seed.js
@@ -19,8 +19,15 @@ async function seedDB() {
     }
 }
 
-function clearDB() {
-    console.log('desde clearDB')
+async function clearDB() {
+    try {
+        await Services.deleteMany()
+        console.log(colors.red.bold('Se eliminaron los datos'))
+        process.exit()
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
 }
 
 if(process.argv[2] === '--import') {

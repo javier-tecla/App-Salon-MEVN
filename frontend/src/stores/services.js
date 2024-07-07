d10b002f8953625781f806a60ad3df86760ed43b
javierBorjas <cristman11@gmail.com>
2024-07-07 17:07:04 -0300

    Mostrando todos los servicios

--- frontend/src/stores/services.js
+++ frontend/src/stores/services.js
@@ -4,16 +4,18 @@ import ServicesAPI from '../api/ServicesAPI'
 
 export const useServicesStore = defineStore('services', () => {
 
+    const services = ref([])
+
     onMounted(async () => {
         try {
             const { data } = await ServicesAPI.all()
-            console.log(data)
+            services.value = data
         } catch (error) {
             console.log(error)
         }
     })
 
     return {
-
+        services
     }
 })

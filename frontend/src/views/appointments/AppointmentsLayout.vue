a7bbda0d968b7584c044f5ec748ac18ea17e92a3
javierBorjas <cristman11@gmail.com>
2024-07-07 00:04:07 -0300

    Comenzando con el Layout de Citas

--- frontend/src/views/appointments/AppointmentsLayout.vue
+++ frontend/src/views/appointments/AppointmentsLayout.vue
@@ -1,12 +1,38 @@
-import { RouterView } from 'vue-router';
+import { RouterView, RouterLink } from 'vue-router';
 <script setup>
         
 </script>
 
 
 <template>
-    <div>
-        <h1>AppointmentLayout</h1>
+    <div class="flex justify-between">
+        <h1 class="text-2xl lg:text-6xl font-black text-white">App Salón</h1>
+
+        <div class="flex flex-col space-y-5">
+            <div class="flex gap-2 items-center">
+                <p class="text-white text-right">Hola: Usuario</p>
+
+                <button
+                    type="butto"
+                    class="bg-red-600 hover:bg-red-700 p-2 text-white uppercase text-xs font-extrabold rounded-lg"
+                >
+                    Cerrar Sesión
+                </button>
+            </div>
+
+            <nav class="flex gap-2 items-center justify-end">
+                <button
+                    class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg"
+                >Mis Citas</button>
+
+                <RouterLink
+                    :to="{name: 'new-appointment'}"
+                    class="p-3 text-gray-200 uppercase text-xs font-black rounded-lg bg-blue-700"
+                >
+                    Nueva Cita
+                </RouterLink>
+            </nav>
+        </div>
     </div>
 
     <main>

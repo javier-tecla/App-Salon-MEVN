39748d1ec7b38f8690b52977f5fede89f6d69a45
javierBorjas <cristman11@gmail.com>
2024-07-06 21:51:17 -0300

    Insertando Registros de forma masiva con insertMany

--- backend/data/seed.js
+++ backend/data/seed.js
@@ -1,7 +1,22 @@
+import dotenv from 'dotenv'
+import colors from 'colors'
+import { db } from '../config/db.js'
+import Services from '../models/Services.js'
+import { services } from './beautyServices.js'
 
+dotenv.config()
 
-function seedDB() {
-    console.log('desde seedDB')
+await db()
+
+async function seedDB() {
+    try {
+        await Services.insertMany(services)
+        console.log(colors.green.bold('Se agregaron los datos correctamente'))
+        process.exit()
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
 }
 
 function clearDB() {

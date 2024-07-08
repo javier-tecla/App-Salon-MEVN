792448ca2e96b99a25bf137c0f43e56d2c5f8f1c
javierBorjas <cristman11@gmail.com>
2024-07-07 21:58:49 -0300

    Evitar Usuarios Duplicados

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 import User from "../models/User.js";
 
 const register = async (req, res) => {
+
+    // Valida todos los campos
   if (Object.values(req.body).includes("")) {
     const error = new Error("Todos los campos son obligatorios");
 
@@ -8,6 +10,20 @@ const register = async (req, res) => {
       msg: error.message,
     });
   }
+
+  const { email, password, name } = req.body
+  // Evitar registros duplicados
+  const userExists = await User.findOne({ email })
+  if(userExists) {
+    const error = new Error("Usuario ya registrado");
+
+    return res.status(400).json({
+      msg: error.message,
+    });
+  }
+
+  // Validar la extensión del password
+  
 
     try {
         const user = new User(req.body)

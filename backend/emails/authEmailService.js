de5db394f4baa1c863e8a345c7142bdb1ad3b1c0
javierBorjas <cristman11@gmail.com>
2024-07-07 23:33:40 -0300

    Enviar email de prueba

--- backend/emails/authEmailService.js
+++ backend/emails/authEmailService.js
@@ -1,5 +1,21 @@
 import { createTransport } from "../config/nodemailer.js"
 
 export async function sendEmailVerification() {
-    console.log('desde sendEmailVerification')
+    const transporter = createTransport(
+        "sandbox.smtp.mailtrap.io",
+        2525,
+       "e507a3259a8be6",
+       "69bdd5ec5fb35e"
+    )
+
+    // Enviar el mail
+    const info = await transporter.sendMail({
+        from: 'AppSalon',
+        to: 'correo@correo.com',
+        subject: "AppSalon - Confirma tu cuenta",
+        text: "AppSalon - Confirma tu cuenta",
+        html: 'Probando email'
+    })
+
+    console.log('Mensaje enviado', info.messageId)
 }

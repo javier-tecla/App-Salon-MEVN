3aec2130deef14d97cad9a1002edf6f7d6517abd
javierBorjas <cristman11@gmail.com>
2024-07-10 20:38:02 -0300

    Registrando Usuarios desde Vue.js

--- frontend/src/views/auth/RegisterView.vue
+++ frontend/src/views/auth/RegisterView.vue
@@ -1,5 +1,14 @@
 <script setup>
+    import AuthAPI from '../../api/AuthAPI';
 
+
+    const handleSubmit = async ({password_confirm, ...data}) => {
+        try {
+            await AuthAPI.register(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
 </script>
 
 
@@ -11,6 +20,7 @@
         type="form"
         :actions="false"
         incomplete-message="No se pudo enviar, revisa las notificaciones"
+        @submit="handleSubmit"
     >
         <FormKit
             type="text"

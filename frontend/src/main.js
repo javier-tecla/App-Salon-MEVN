db32c0d296baee2da57792ca65252736567de888
javierBorjas <cristman11@gmail.com>
2024-07-10 20:48:43 -0300

    Implementando Notificaciones Toast

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,23 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { plugin, defaultConfig } from '@formkit/vue'
+import { useToast } from 'vue-toast-notification'
 import config from '../formkit.config'
 
 import App from './App.vue'
 import router from './router'
+
+import "vue-toast-notification/dist/theme-sugar.css"
+
+const toast = useToast({
+    duration: 5000,
+    position: 'top-right'
+})
+
+toast.open({
+    message: 'Probando Toast',
+    type: 'success'
+})
 
 const app = createApp(App)
 

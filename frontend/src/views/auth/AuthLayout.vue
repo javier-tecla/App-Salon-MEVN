ee3cedc1ea06a003780a3298135ca26248c8bdf3
javierBorjas <cristman11@gmail.com>
2024-07-10 19:26:21 -0300

    Mostrando los enlaces hacia la pagina de Registro y Autenticacion

--- frontend/src/views/auth/AuthLayout.vue
+++ frontend/src/views/auth/AuthLayout.vue
@@ -1,12 +1,28 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 
+
+    const authRoutes = [
+        { name: 'register', text: 'Crear una cuenta'},
+        { name: 'login', text: 'Iniciar Sesión'},
+    ]
 </script>
 
 
 <template>
-    <div>
-        <h1>Desde Auth Layout</h1>
+    <div class="mx-auto lg:w-4/5 mt-20">
 
         <RouterView />
+
+        <nav class="mt-10 flex flex-col items-center space-y-5 lg:flex-row lg:justify-between lg:space-y-0">
+            <RouterLink
+                v-for="authRoute in authRoutes"
+                class="uppercase font-bold text-white"
+                :to="{name: authRoute.name}"
+            >{{ authRoute.text }}</RouterLink>
+
+        </nav>
+
+
     </div>
 </template>

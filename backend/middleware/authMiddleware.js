da334c5923453dbac8badbdcd0c08a4b09260352
javierBorjas <cristman11@gmail.com>
2024-07-12 20:20:22 -0300

    Verificar un JWT EN EL servidor

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,8 +1,16 @@
+import jwt from 'jsonwebtoken'
 
 const authMiddleware = async ( req, res, next ) => {
 
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        console.log('Si hay Token')
+        try {
+            const token = req.headers.authorization.split(' ')[1]
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+            console.log(decoded)
+        } catch {
+            const error = new Error('Token no válido')
+            res.status(403).json({msg: error.message})
+        }
     } else {
         const error = new Error('Token no válido o inexistente')
         res.status(403).json({msg: error.message})

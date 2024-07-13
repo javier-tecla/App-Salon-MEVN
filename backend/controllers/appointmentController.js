669be4699579bd75c134e576f2d5df7f063933ec
javierBorjas <cristman11@gmail.com>
2024-07-13 16:43:14 -0300

    Obtener citas por fecha - Backend

--- backend/controllers/appointmentController.js
+++ backend/controllers/appointmentController.js
@@ -1,3 +1,4 @@
+import { parse, formatISO, startOfDay, endOfDay, isValid } from 'date-fns'
 import Appointment from "../models/Appointment.js"
 
 const createAppointment = async (req, res) => {
@@ -16,6 +17,31 @@ const createAppointment = async (req, res) => {
     }
 }
 
+const getAppointmentsByDate = async (req, res) => {
+    const { date } = req.query
+
+    const newDate = parse(date, 'dd/MM/yyyy', new Date())
+
+    if(!isValid(newDate)) {
+        const error = new Error("Fecha no válida");
+
+        return res.status(400).json({
+          msg: error.message,
+        });
+    }
+
+    const isoDate = formatISO(newDate)
+
+    const appointments = await Appointment.find({ date: {
+        $gte : startOfDay(new Date(isoDate)),
+        $lte: endOfDay(new Date(isoDate))
+    }}).select('time')
+
+    res.json(appointments)
+
+}
+
 export {
-    createAppointment
+    createAppointment,
+    getAppointmentsByDate
 }

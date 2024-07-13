3b118c60a9f3cefdfd9aa134a8c10e26503e099e
javierBorjas <cristman11@gmail.com>
2024-07-13 00:24:13 -0300

    Definiendo Routing y Controller para Citas

---
+++ backend/controllers/appointmentController.js
@@ -0,0 +1,13 @@
+
+
+const createAppointment = async (req, res) => {
+    try {
+        console.log('desde createAppointment')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export {
+    createAppointment
+}
--- backend/routes/appointmentRoutes.js
+++ backend/routes/appointmentRoutes.js
@@ -1,7 +1,11 @@
 import express from 'express'
+import { createAppointment } from '../controllers/appointmentController.js'
+import authMiddleware from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
+router.route('/')
+    .post(authMiddleware, createAppointment)
 
 
 

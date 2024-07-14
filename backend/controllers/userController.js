724c8305d67c6463c5f3ddb064b539e6c4130cb2
javierBorjas <cristman11@gmail.com>
2024-07-14 14:07:23 -0300

    Obtener las citas del usuario autenticado y evitar que otros puedan verlas

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,27 @@
+import Appointment from "../models/Appointment.js"
 
 const getUserAppointments = async (req, res) => {
-    console.log('desde getUserAppoinyments')
+    const { user } = req.params
+
+    
+    if(user !== req.user._id.toString() ) {
+        const error = new Error('Acceso Denegado')
+        return res.status(400).json({msg: error.message})
+    }
+
+    try {
+        const appointments = await Appointment.find({ 
+            user ,
+            date: {
+                $gte : new Date()
+            }
+        }).populate('services')
+
+
+        res.json(appointments)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 

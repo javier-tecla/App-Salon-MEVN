7ae6a50b6b3660806a43508f88cdebc08277c6f2
javierBorjas <cristman11@gmail.com>
2024-07-17 22:13:36 -0300

    Mostrando la información del usuario en las citas

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,7 @@ const getUserAppointments = async (req, res) => {
 
     try {
         const query = req.user.admin ? { date: { $gte : new Date() } } : { user, date: { $gte : new Date() } }
-        const appointments = await Appointment.find(query).populate('services').sort({date: 'asc'})
+        const appointments = await Appointment.find(query).populate('services').populate({path: 'user', select: 'name email'}).sort({date: 'asc'})
 
 
         res.json(appointments)
